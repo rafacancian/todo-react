b00504f593fdc4b300c665061120721b57ef383b
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-18 17:30:08 +0000

    keyup to add and search task

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,13 +4,23 @@ import IconButton from '../template/iconButton'
 
 export default props => {
 
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
      return (
             <div role='form' className='todoForm'>
                 <Grid cols='12 9 10'>
                     <input id='description' className='form-control'
                         placeholder='Add Task'
                         onChange={props.handleChange}
-                        value={props.description}></input>
+                        value={props.description}
+                        onKeyUp={keyHandler}>
+                    </input>
                 </Grid>
                 <Grid cols='12 3 2'>
                    <IconButton style='primary' icon='plus'
