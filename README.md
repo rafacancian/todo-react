e40998a424306d0b9399aad589353779d4f42be2
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-15 11:29:41 +0000

    screenshot views

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 TO DO task management system
 Project create to study frontend technologies
 
+##RUN
+`npm run dev`
+
 ##React
 React is an open source JavaScript library focused on creating user interfaces on web pages
 React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. 
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -8,14 +8,14 @@ export default props => {
            const list = props.list || []
 
            return list.map(task => (
-               <tr key={task.id}>
+               <tr key={task.id} className="text-center">
                    <td>
                        <IconButton style='default' icon='check' hide={task.done} onClick={() => props.handleMarkAsDone(task)}></IconButton>
                        <IconButton style='success' icon='check' hide={!task.done} onClick={() => props.handleMarkAsPending(task)}></IconButton>
                    </td>
-                   <td className={task.done ? 'markedAsDone text-success' : ''}>{task.description}</td>
-                       <td className={task.done ? 'markedAsDone text-success' : ''}>{task.creation_date}</td>
-                       <td className={task.done ? 'markedAsDone text-success' : ''}>{task.due_date}</td>
+                   <td className={task.done ? 'markedAsDone text-success text-left' : 'text-left'}>{task.description}</td>
+                   <td className={task.done ? 'markedAsDone text-success' : ''}>{task.creation_date}</td>
+                   <td className={task.done ? 'markedAsDone text-success' : ''}>{task.due_date}</td>
                    <td>
                        <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(task.id)}></IconButton>
                    </td>
@@ -27,11 +27,11 @@ export default props => {
            <table className='table table-striped'>
                <thead>
                    <tr>
-                       <th className='tableActions'></th>
-                       <th>DESCRIPTION</th>
-                       <th>CREATION DATE</th>
-                       <th>DUE DATE</th>
-                       <th className='tableActions'></th>
+                       <th className='tableActions text-center'>STATUS</th>
+                       <th className='text-center'>DESCRIPTION</th>
+                       <th className='text-center'>CREATION DATE</th>
+                       <th className='text-center'>DUE DATE</th>
+                       <th className='tableActions'>ACTION</th>
                    </tr>
                </thead>
                <tbody>
